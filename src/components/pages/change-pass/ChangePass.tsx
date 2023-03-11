70603e5b24aa580c30e1e15c2a070de6604e0d95
igorSimash <vigorochok@gmail.com>
2023-03-11 19:30:38 +0200

    change-pass page responsive

--- src/components/pages/change-pass/ChangePass.tsx
+++ src/components/pages/change-pass/ChangePass.tsx
@@ -6,7 +6,8 @@ import SubmitError from "../../UI/errors/SubmitError";
 import {useTranslation} from "react-i18next";
 import {useNavigate} from "react-router-dom";
 import {postData} from "../../../utils/fetch-data/PostData";
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {useWindowSize} from "../../../hooks/useWindowSize";
 const ChangePass: React.FC = () => {
     const [oldPass, setOldPass] = useState('');
     const [newPass, setNewPass] = useState('');
@@ -14,6 +15,7 @@ const ChangePass: React.FC = () => {
     const [error, setError] = useState('');
     const {t} = useTranslation(['error', 'changePassword']);
     const navigate = useNavigate()
+    const [width] = useWindowSize();
     const handleClick = () => {
         if (newPass !== repNewPass)
             setError('passNotMatch');
@@ -35,23 +37,28 @@ const ChangePass: React.FC = () => {
     }
 
     return (
-        <div className={'h-screen flex'}>
-            <div className={'w-7/12'}>
-                <div className={'flex justify-center items-center h-full'}>
-                    <div className={'flex flex-col gap-14'}>
-                        <span className={'text-6xl text-solidBlue'}>{t('title', {ns: 'changePassword'})}</span>
-                        <div className={'flex flex-col gap-2'}>
-                            <span className={'text-xl mb-3'}>{t('passContainTitle', {ns: 'changePassword'})}</span>
-                            <LightText>{t('contain8Symb', {ns: 'changePassword'})}</LightText>
-                            <LightText>{t('containOneUpper', {ns: 'changePassword'})}</LightText>
-                            <LightText>{t('containOneLower', {ns: 'changePassword'})}</LightText>
-                            <LightText>{t('noCyrillic', {ns: 'changePassword'})}</LightText>
+        <div className={'h-screen flex relative justify-center '}>
+            <ArrowBackIcon className={'absolute top-10 left-10 cursor-pointer'} onClick={() => navigate('/todos')}/>
+            {width > 875
+                &&
+                <div className={'w-7/12'}>
+                    <div className={'flex justify-center items-center h-full'}>
+                        <div className={'flex flex-col gap-14'}>
+                            <span
+                                className={'text-6xl md:text-5xl text-solidBlue'}>{t('title', {ns: 'changePassword'})}</span>
+                            <div className={'flex flex-col gap-2'}>
+                                <span
+                                    className={'text-xl mb-3 md:mb-1'}>{t('passContainTitle', {ns: 'changePassword'})}</span>
+                                <LightText>{t('contain8Symb', {ns: 'changePassword'})}</LightText>
+                                <LightText>{t('containOneUpper', {ns: 'changePassword'})}</LightText>
+                                <LightText>{t('containOneLower', {ns: 'changePassword'})}</LightText>
+                                <LightText>{t('noCyrillic', {ns: 'changePassword'})}</LightText>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div>}
             <div className={'w-5/12 flex flex-col justify-center items-center'}>
-                <form className={'flex flex-col items-center gap-10 w-96'}>
+                <form className={'flex flex-col items-center gap-10 w-96 md:w-80 s:w-80'}>
                     <div className={'flex flex-col items-start gap-8 w-full'}>
                         <PasswordInput onChange={e => setOldPass(e.target.value)}
                                        label={t('oldPass', {ns: 'changePassword'})} id={'old-pass'}/>
